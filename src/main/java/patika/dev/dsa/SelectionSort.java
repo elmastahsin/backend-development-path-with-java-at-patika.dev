88edf07958ea9497f31523d2db65df386b04c3db
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-04-10 04:42:35 +0300

    dsa project implemented

---
+++ src/main/java/patika/dev/dsa/SelectionSort.java
@@ -0,0 +1,27 @@
+package patika.dev.dsa;
+
+public class SelectionSort {
+
+    public static int[] selectionSort(int[] array) {
+        int[] result = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]) {
+                    min = j;
+                }
+            }
+            swap(array, min, i);
+
+        }
+        return array;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+}
